e46bd05bcd2415435d3ff4bf2140b3f5ac081218
Jaepil Jeong <jaepil@deepsearch.com>
2021-05-06 15:14:36 +0900

    Update deduce_tf_type() to support string and bool types

--- include/cppflow/datatype.h
+++ include/cppflow/datatype.h
@@ -83,32 +83,37 @@ namespace cppflow {
      * @return The TensorFlow type of T
      */
     template<typename T>
-    TF_DataType deduce_tf_type() {
-        if (std::is_same<T, float>::value)
+    inline constexpr TF_DataType deduce_tf_type() {
+        if constexpr (std::is_same_v<T, float>)
             return TF_FLOAT;
-        if (std::is_same<T, double>::value)
+        if constexpr (std::is_same_v<T, double>)
             return TF_DOUBLE;
-        if (std::is_same<T, int32_t >::value)
+        if constexpr (std::is_same_v<T, int32_t>)
             return TF_INT32;
-        if (std::is_same<T, uint8_t>::value)
+        if constexpr (std::is_same_v<T, uint8_t>)
             return TF_UINT8;
-        if (std::is_same<T, int16_t>::value)
+        if constexpr (std::is_same_v<T, int16_t>)
             return TF_INT16;
-        if (std::is_same<T, int8_t>::value)
+        if constexpr (std::is_same_v<T, int8_t>)
             return TF_INT8;
-        if (std::is_same<T, int64_t>::value)
+        if constexpr (std::is_same_v<T, int64_t>)
             return TF_INT64;
-        if (std::is_same<T, unsigned char>::value)
+        if constexpr (std::is_same_v<T, unsigned char>
+                    || std::is_same_v<T, bool>)
             return TF_BOOL;
-        if (std::is_same<T, uint16_t>::value)
+        if constexpr (std::is_same_v<T, uint16_t>)
             return TF_UINT16;
-        if (std::is_same<T, uint32_t>::value)
+        if constexpr (std::is_same_v<T, uint32_t>)
             return TF_UINT32;
-        if (std::is_same<T, uint64_t>::value)
+        if constexpr (std::is_same_v<T, uint64_t>)
             return TF_UINT64;
+        if constexpr (std::is_same_v<T, std::string> 
+                    || std::is_same_v<T, std::string_view>)
+            return TF_STRING;
 
         // decode with `c++filt --type $output` for gcc
-        throw std::runtime_error{"Could not deduce type! type_name: " + std::string(typeid(T).name())};
+        throw std::runtime_error {"Could not deduce type! type_name: "
+                                + std::string {typeid(T).name()}};
     }
 
     /**
